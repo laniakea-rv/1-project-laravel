ae5b260fd73a188cc182d3cfbf1fc3a3dc6dfb3a
bram35 <rv669912@edu.rocrivor.nl>
2026-06-30 14:10:09 +0200

    migration van videos en voortgang

---
+++ examen-project/database/migrations/2026_06_30_113454_create_voortgangen_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('voortgang_video', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('voortgang_id')
+                ->constrained('voortgangen')
+                ->onDelete('cascade');
+
+            $table->foreignId('video_id')
+                ->constrained('videos')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('voortgangen');
+    }
+};
