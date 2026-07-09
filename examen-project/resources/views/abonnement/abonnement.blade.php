d77a6d516dcd0b49ee8a1e0671b4b40166764dd4
laniakea <rv669647@edu.rocrivor.nl>
2026-07-09 22:59:50 +0200

    feat: implemented abonnement opzeggen
    gemaakt zodat je je huidige abonnement kan zien en opzeggen, je kan ook niet meerdere actieve abonnementen tegelijk nemen of abonnementen van anderen stop zetten

--- examen-project/resources/views/abonnement/abonnement.blade.php
+++ examen-project/resources/views/abonnement/abonnement.blade.php
@@ -1,20 +1,36 @@
 @extends ("layouts.app")
 @section("content")
-<body>
-<div>
-  @foreach ($abonnement as $item)
-    <tr>
-      <td>{{ $item->id }}</td>
-      <td>{{ $item->naam }}</td>
-        <td>{{ $item->beschrijving }}</td>
-      <td>{{ $item->prijs }}</td>
-      <td> <form action="{{ route('saveUserAbonnement')}}" method="post">
-        @csrf
-        <input type="hidden" name="id" value="{{ $item->id }}">
-        <button type="submit">aanschaffen</button></form></td>
-    </tr>
-    <br>
-  @endforeach
-</div>
-</body>
+
+  <body>
+    <div>
+      @if(session('error'))
+        <p>{{ session('error') }}</p>
+      @endif
+      @foreach ($abonnementen as $item)
+        <tr>
+          <td>{{ $item->naam }}</td>
+          <td>{{ $item->beschrijving }}</td>
+          <td>{{ $item->prijs }}</td>
+          <td>
+            <form action="{{ route('saveUserAbonnement')}}" method="post">
+              @csrf
+              <input type="hidden" name="id" value="{{ $item->id }}">
+              <button type="submit">aanschaffen</button>
+            </form>
+          </td>
+        </tr>
+        <br>
+      @endforeach
+      @if($huidigAbonnement)
+        <p>jouw abonnement nu is {{ $huidigAbonnement->abonnementtype->naam }}</p>
+        <form action="{{ route('opzeggenUserAbonnement') }}" method="post">
+          @method('PUT')
+          @csrf
+          <button type="submit">abonnement opzeggen</button>
+        </form>
+      @else
+        <p>je hebt nog geen actief abonnement, laten we dat veranderen</p>
+      @endif
+    </div>
+  </body>
 @endsection
