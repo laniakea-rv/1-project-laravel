7de2c8bc7d1a41689f038af991987e6b9aa4d7f9
Sop <sophie.kicken@outlook.com>
2026-08-11 16:18:16 +0200

    Feat: abonnement opzeggen en  aanschaffen gefixed

--- examen-project/resources/views/abonnement/abonnement.blade.php
+++ examen-project/resources/views/abonnement/abonnement.blade.php
@@ -1,41 +1,73 @@
-@extends ("layouts.app")
-@section("content")
+@extends('layouts.app')
+@section('content')
+<div class="flex justify-center">
+    <div class="bg-gray-300 text-black mt-10 w-6/7 rounded shadow pt-3">
+        @if(session('error'))
+            <p class="text-red-600 p-3">
+                {{ session('error') }}
+            </p>
+        @endif
+        @if(session('success'))
+            <p class="text-green-600 p-3">
+                {{ session('success') }}
+            </p>
+        @endif
+        @foreach ($abonnementen as $item)
 
-  <body class="">
-    <div class="flex justify-center">
-    <div class='bg-gray-300 text-black mt-10 w-6/7 rounded shadow pt-3 '>
-      @if(session('error'))
-        <p>{{ session('error') }}</p>
-      @endif
-      @foreach ($abonnementen as $item)
-      <div class="flex justify-center">
-      <div class=" bg-gray-100 mt-3 w-5/6 rounded shadow ">
-        <tr>
-          <td>{{ $item->naam }}</td>
-          <td>{{ $item->beschrijving }}</td>
-          <td>€{{ $item->prijs }}</td>
-          <td>
-            <form action="{{ route('saveUserAbonnement')}}" method="post">
-              @csrf
-              <input type="hidden" name="id" value="{{ $item->id }}">
-              <button type="submit">aanschaffen</button>
-          </td>
-        </tr>
-      </div>
-      </div>
-        <br>
-      @endforeach
-      @if($huidigAbonnement)
-        <p>jouw abonnement nu is {{ $huidigAbonnement->abonnementtype->naam }}</p>
-        <form action="{{ route('opzeggenUserAbonnement') }}" method="post">
-          @method('PUT')
-          @csrf
-          <button type="submit">abonnement opzeggen</button>
-        </form>
-      @else
-        <p>je hebt nog geen actief abonnement, laten we dat veranderen</p>
-      @endif
+            <div class="flex justify-center">
+                <div class="bg-gray-100 mt-3 w-5/6 rounded shadow p-4">
+                    <p>
+                        <strong>{{ $item->naam }}</strong>
+                    </p>
+                    <p>
+                        {{ $item->beschrijving }}
+                    </p>
+                    <p>
+                        €{{ $item->prijs }}
+                    </p>
+                    <form action="{{ route('saveUserAbonnement') }}" method="POST">
+                        @csrf
+                        <input
+                            type="hidden"
+                            name="id"
+                            value="{{ $item->id }}"
+                        >
+                        <button type="submit">
+                            Aanschaffen
+                        </button>
+                    </form>
+
+                </div>
+            </div>
+        @endforeach
+        @if($huidigAbonnement)
+            <div class="p-4">
+                <p>
+                    Jouw abonnement nu is:
+                    {{ $huidigAbonnement->abonnementtype->naam }}
+                </p>
+
+                <form
+                    action="{{ route('opzeggenUserAbonnement') }}"
+                    method="POST"
+                >
+                    @csrf
+                    @method('PUT')
+
+                    <button class="bg-red-800 px-3 text-white rounded" type="submit">
+                        Abonnement opzeggen
+                    </button>
+                </form>
+            </div>
+        @else
+
+            <p class="p-4">
+                Je hebt nog geen actief abonnement, laten we dat veranderen.
+            </p>
+
+        @endif
+
     </div>
-  </div>
-  </body>
+</div>
+
 @endsection
